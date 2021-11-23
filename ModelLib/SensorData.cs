2ac0732092b61fe8080d54f6bef2039f433bbeb0
ChristopherLoeve <74707527+ChristopherLoeve@users.noreply.github.com>
2021-11-23 10:54:58 +0100

    SensorData class implemented in ModelLib

--- ModelLib/SensorData.cs
+++ ModelLib/SensorData.cs
@@ -8,5 +8,45 @@ namespace ModelLib
 {
     class SensorData
     {
+        public int Id { get; set; }
+        public string SensorName { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public int Pressure { get; set; }
+        
+
+        public SensorData()
+        {
+
+        }
+
+        public SensorData(int id, string name, double temperature, double humidity, int pressure)
+        {
+            Id = id;
+            SensorName = name;
+            Temperature = temperature;
+            Humidity = humidity;
+            Pressure = pressure;
+        }
+
+        public override string ToString()
+        {
+            return $"{SensorName} - Data {Id}\nTemp: {Temperature}\nHumidity: {Humidity}\nPressure: {Pressure} ";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SensorData data &&
+                   Id == data.Id &&
+                   SensorName == data.SensorName &&
+                   Temperature == data.Temperature &&
+                   Humidity == data.Humidity &&
+                   Pressure == data.Pressure;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, SensorName, Temperature, Humidity, Pressure);
+        }
     }
 }
