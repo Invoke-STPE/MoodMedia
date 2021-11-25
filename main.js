8eef0b7317d7afb79498b19c7d765099469ff83b
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2021-11-25 11:09:15 +0100

    SpotifyLogin
    You can now login and authenticate with Spotify

--- main.js
+++ main.js
@@ -125,11 +125,16 @@ const sunComponent = {
               `,
 };
 
+
 const app = {
   data() {
     return {
       rain: true,
       login: false,
+      client_id: "8c68d039b2544b31a1064152fbb24c51",
+      scopes: ["user-read-private", "user-read-email"],
+      redirect_uri: "http://127.0.0.1:5501/index.html",
+      me: null,
     };
   },
   components: {
@@ -138,13 +143,39 @@ const app = {
     "sun-component": sunComponent,
   },
   methods: {
-    toggleLogin() {
-      this.login = !this.login;
-    },
-    works() {
-      console.log("works");
-    },
+    spotfiyAuthentication() {
+      let popup = window.open(
+        `https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.scopes}&show_dialog=true`,
+        "Login with Spotify",
+        "width=800,height=600"
+      );
+
+      window.spotifyCallback = (payload) => {
+        this.login = true;
+        popup.close();
+
+        fetch('https://api.spotify.com/v1/me', {
+          headers: {
+            'Authorization': `Bearer ${payload}`
+          }
+        }).then(response => {
+          return response.json();
+        }).then(data => {
+          this.me = data;
+        })
+      }
+    }
   },
+
+  mounted() {
+    this.token = window.location.hash.substr(1).split("&")[0].split("=")[1]
+
+    if(this.token) {
+      //alert(this.token)
+      
+      window.opener.spotifyCallback(this.token);
+    }
+  }
 };
 
 // weather.mount("#weather");
