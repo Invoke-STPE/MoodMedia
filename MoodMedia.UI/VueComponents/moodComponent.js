54a4320419df4b017e52a7b50b2820c7b07bf74a
Christopher <38313657+NattyPew@users.noreply.github.com>
2021-11-30 13:23:52 +0100

    added snow effect that does not work

--- MoodMedia.UI/VueComponents/moodComponent.js
+++ MoodMedia.UI/VueComponents/moodComponent.js
@@ -31,7 +31,7 @@ app.component('mood-component', {
       else if (response.temperature < 15) {
         this.mood = "cold"
         this.cold= true
-        if (response.humidity >= 80) {
+        if (response.humidity >= 20) {
           this.mood += " and rainy"
           this.rain = true
         }
@@ -39,7 +39,7 @@ app.component('mood-component', {
       else if (response.temperature <= 22) {
         this.mood = "nice" 
         this.sunny = true
-        if (response.humidity >= 80) {
+        if (response.humidity >= 20) {
           this.mood += " but rainy"
           this.rain = true
         }
@@ -47,7 +47,7 @@ app.component('mood-component', {
       else {
         this.mood = "hot"
         this.sunny = true
-        if (response.humidity >= 80) {
+        if (response.humidity >= 20) {
           this.mood += " but rainy"
           this.rain = true
         }
@@ -58,7 +58,6 @@ app.component('mood-component', {
   <div class="text-light"><p>Freezing: {{freezing}} Cold: {{cold}} Sun: {{sunny}} | Rain: {{rain}} Snow: {{snow}}</p></div>
   <sun-component v-if="sunny"></sun-component>
   <rain-component v-if="rain"></rain-component>
-
   <weather-information-box v-if="weather" v-bind:weather=weather v-bind:mood=mood></weather-information-box>
   <mood-button @getMood="setMood"> </mood-button>
   <br/>
