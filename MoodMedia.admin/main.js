8022e6d595d3fd0c0f1a454285b1c1c12cf99be3
Invoke-STPE <47424622+Invoke-STPE@users.noreply.github.com>
2021-11-30 08:24:41 +0100

    admini work

--- MoodMedia.admin/main.js
+++ MoodMedia.admin/main.js
@@ -1,12 +1,37 @@
 const app = Vue.createApp({
   data() {
     return {
+      sensorData: Seed.sensorData,
       login: true,
+      dateTimeSort: true,
     };
+  },
+  computed: {
+    sortedDateTimes() {
+      if (this.dateTimeSort) {
+        //   return this.sensorData
+        //     .slice(0)
+        //     .sort((a, b) => new Date(a) - new Date(b));
+        // }
+        let tempArray = this.sensorData.sort(
+          (a, b) => new Date(a) - new Date(b)
+        );
+        return tempArray;
+      }
+    },
   },
   methods: {
     logout() {
       return null;
+    },
+    formatDate(date) {
+      let formattedDate = "";
+      formattedDate = new Date(date).toLocaleString();
+
+      for (let index = 0; index < formattedDate.length; index++) {
+        formattedDate = formattedDate.replace(".", ":");
+      }
+      return formattedDate.replace(",", " ");
     },
   },
 });
