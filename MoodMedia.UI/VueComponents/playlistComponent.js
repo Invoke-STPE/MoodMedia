f605ec838a91025d2a848f3d71264b4880cf76cb
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2021-12-01 13:07:47 +0100

    Placeholder
    removed irrelevant placeholder attribute

--- MoodMedia.UI/VueComponents/playlistComponent.js
+++ MoodMedia.UI/VueComponents/playlistComponent.js
@@ -15,19 +15,19 @@ app.component("playlist-component", {
           <div class="modal-body bg-dark">
           Alle playliste indstillinger
             <div>
-              <select placeholder="happyPlaylist" name="playlist" id="playlist">
+              <select name="playlist" id="playlist">
                 <option value="" disabled selected>Select a happy playlist </option>
                 <option value="playlist">Banger</option>
               </select>
             </div>
             <div>
-              <select placeholder="okPlaylist" name="playlist" id="playlist">
+              <select name="playlist" id="playlist">
                 <option value="" disabled selected>Select a ok playlist </option>
                 <option value="playlist">wackass</option>
               </select>
             </div>
             <div>
-              <select placeholder="sadPlaylist" name="playlist" id="playlist">
+              <select name="playlist" id="playlist">
                 <option value="" disabled selected>Select a sad playlist </option>
                 <option value="playlist">sad af</option>
               </select>
